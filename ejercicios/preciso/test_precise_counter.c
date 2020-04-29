7ed616b13007368e6f4e734a3ae81a73095fa273
Luis Giraldo <luis.giraldo10@udea.edu.co>
2020-04-28 23:59:00 -0500

    FEAT: test_precise_counter

--- ejercicios/preciso/test_precise_counter.c
+++ ejercicios/preciso/test_precise_counter.c
@@ -4,68 +4,91 @@
 #include <sys/time.h>
 #include "counter.h"
 
-
 /* structs */
 // code here (if you required it)...
 
 /* start_routine header */
-// code here...
+void *incrementCounter(void *);
 
 /* Global variables */
 // code here (if you required it)...
+counter_t counter;
+int MAXCNT;
+int NUMTHREADS;
 
-int main(int argc, char *argv[]) { 
+int main(int argc, char *argv[])
+{
 
     /* get the command's line parameters */
-    // code here...
+    if (argc < 3)
+    {
+        printf("Debe llamar al programa con el maximo del contador y el número de hilos.\n");
+        return 1;
+    }
 
+    MAXCNT = atoi(argv[1]);
+    NUMTHREADS = atoi(argv[2]);
+    int maxCntPerThread[NUMTHREADS];
 
     /* Declaration of struct timeval variables */
-    // code here...
-
+    struct timeval start_tv, end_tv;
 
     /* Initializing conter */
-    // code here...
-
+    init(&counter);
 
     /* Threads handlers */
-    // code here...
-
+    pthread_t threads[NUMTHREADS];
 
     /* Thread creation */
-    // code here...
+    int res = MAXCNT % NUMTHREADS;
+    int threadsMax = MAXCNT / NUMTHREADS;
 
+    for (int i = 0; i < NUMTHREADS; i++)
+    {
+        maxCntPerThread[i] = threadsMax;
+
+        if (res > 0 && i < res)
+        {
+            maxCntPerThread[i] += 1;
+        }
+    }
 
     /* Time starts counting */
-    // code here...
-
+    gettimeofday(&start_tv, NULL);
 
     /* Creating a Threads */
-    // code here...
-   
+    for (int i = 0; i < NUMTHREADS; i++)
+    {
+        pthread_create(&threads[i], NULL, incrementCounter, ((void *)&maxCntPerThread[i]));
+    }
 
     /* Threads joins */
-    // code here...
-
-
-    /* Time stops counting here */
-    // code here...
-
+    for (int i = 0; i < NUMTHREADS; i++)
+    {
+        pthread_join(threads[i], NULL);
+    }
 
     /* get the end time */
-    // code here...
-    
+    gettimeofday(&end_tv, NULL);
 
     /* get the elapset time (end_time - start_time) */
-    // code here...
-
+    double elapsed_time = (end_tv.tv_sec - start_tv.tv_sec) + (end_tv.tv_usec - start_tv.tv_usec) / 1000000.0;
 
     /* print the results (number threads employed, counter value, elasep time) */
-    // code here...
-
+    printf("La cantidad de hilos es: %d, el valor del contador es: %d, tiempo transcurrido: %lf \n", NUMTHREADS, counter.value, elapsed_time);
 
     return 0;
 }
 
 /* start_routine definition */
-// code here...
+void *incrementCounter(void *param)
+{
+    int max = *(int *)param;
+
+    for (int i = 0; i < max; i++)
+    {
+        increment(&counter);
+    }
+
+    return NULL;
+}
