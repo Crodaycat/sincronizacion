005872a3e5d3356e8bf5a638251bc1eed1ecab7e
Luis Giraldo <luis.giraldo10@udea.edu.co>
2020-05-07 23:21:33 -0500

    FEAT: test_aproximate_counter

--- ejercicios/aproximado/counter.h
+++ ejercicios/aproximado/counter.h
@@ -3,19 +3,19 @@
 #include <stdio.h>
 #include <pthread.h>
 
-#define NUMCPUS 4
+#define NUMCPUS 8
 
-typedef struct __counter_t {
-    int global; // global count
-    pthread_mutex_t glock; // global lock
-    int local[NUMCPUS]; // per-CPU count
+typedef struct __counter_t
+{
+    int global;                     // global count
+    pthread_mutex_t glock;          // global lock
+    int local[NUMCPUS];             // per-CPU count
     pthread_mutex_t llock[NUMCPUS]; // ... and locks
-    int threshold; // update frequency
+    int threshold;                  // update frequency
 } counter_t;
 
-
 void init(counter_t *c, int threshold);
-void update(counter_t *c, int threadID, int amt); 
+void update(counter_t *c, int threadID, int amt);
 int get(counter_t *c);
 
 #endif
--- ejercicios/aproximado/test_aproximate_counter.c
+++ ejercicios/aproximado/test_aproximate_counter.c
@@ -7,71 +7,75 @@
 #define THRESHOLD 1024
 
 /* structs */
-// code here (if you required it)...
 
 /* start_routine header */
-// code here...
+void *increment_counter();
 
 /* Global variables */
-// code here (if you required it)...
+counter_t counter;
+int MAXCNT;
+int NUMTHREADS;
 
-int main(int argc, char *argv[]) { 
+int main(int argc, char *argv[])
+{
 
     /* get the command's line parameters */
-    // code here...
+    if (argc < 3)
+    {
+        printf("Debe llamar al programa con el máximo del contador y el número de hilos.\n");
+        return 1;
+    }
 
+    MAXCNT = atoi(argv[1]);
+    NUMTHREADS = atoi(argv[2]);
 
     /* Declaration of struct timeval variables */
-    // code here...
-
+    struct timeval start_tv, end_tv;
 
     /* Initializing conter */
-    // code here...
-
+    init(&counter, THRESHOLD);
 
     /* Threads handlers */
-    // code here...
-
-
-    /* Thread creation */
-    // code here...
-
+    pthread_t threads[NUMTHREADS];
 
     /* Time starts counting */
-    // code here...
-
+    gettimeofday(&start_tv, NULL);
 
     /* Creating a Threads */
-    // code here...
-   
+    for (int i = 0; i < NUMTHREADS; i++)
+    {
+        pthread_create(&threads[i], NULL, increment_counter, NULL);
+    }
 
     /* Threads joins */
-    // code here...
-
+    for (int i = 0; i < NUMTHREADS; i++)
+    {
+        pthread_join(threads[i], NULL);
+    }
 
     /* Time stops counting here */
-    // code here...
-
-
-    /* get the end time */
-    // code here...
-    
+    gettimeofday(&end_tv, NULL);
 
     /* get the elapset time (end_time - start_time) */
-    // code here...
-
+    double elapsed_time = (end_tv.tv_sec - start_tv.tv_sec) + (end_tv.tv_usec - start_tv.tv_usec) / 1000000.0;
 
     /* print the results (number threads employed, counter value, elasep time) */
-    // code here...
-
+    printf("La cantidad de hilos es: %d, el valor del contador es: %d, tiempo transcurrido: %lf \n", NUMTHREADS, get(&counter), elapsed_time);
 
     return 0;
 }
 
 /* start_routine definition */
-// code here...
+void *increment_counter()
+{
+    int t_id = pthread_self();
+    int value = get(&counter);
 
+    while (value < MAXCNT)
+    {
+        update(&counter, t_id, 1);
+        value = get(&counter);
+    }
 
-
-
-
+    return NULL;
+}
